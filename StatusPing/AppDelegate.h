26c7d9b9be49bf17e86f4993ee4d5abe7448eedb
Diogo Gomes <dgomes@ua.pt>
2013-01-06 21:19:46 +0000

    ver1.0

--- StatusPing/AppDelegate.h
+++ StatusPing/AppDelegate.h
@@ -7,9 +7,21 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#include "SimplePing.h"
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, SimplePingDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (strong, nonatomic) IBOutlet NSMenu *statusMenu;
+@property (strong) IBOutlet NSButton *setButton;
+@property (strong, retain) IBOutlet NSTextField *addressInput;
+@property (strong) IBOutlet NSMenuItem *ipaddress;
+@property (strong, nonatomic) NSStatusItem *statusBar;
+
+@property (strong, atomic) NSString *address;
+
+@property (nonatomic, strong, readwrite) SimplePing *   pinger;
+@property (nonatomic, strong, readwrite) NSTimer *      sendTimer;
+@property (atomic, readwrite) NSInteger unreceivedReplys;
 
 @end
